b012934212d2a0aa25ef08a1dba40c0936e66949
didi <git@d10r.net>
2025-08-04 15:07:07 +0200

    workaround to the Address issue

--- packages/sdk-core/tasks/build-types.sh
+++ packages/sdk-core/tasks/build-types.sh
@@ -15,5 +15,11 @@ fi
 # copy the typechain files over from ethereum-contracts
 cp -r ../ethereum-contracts/typechain-types ./src/typechain-types
 
+# Remove the Address export from typechain-types to avoid conflict with mappedSubgraphTypes
+# OpenZeppelin v5 added a custom error to Address library, giving it a non-empty ABI
+# This causes typechain to generate types for it, but it's not needed for the SDK
+sed -i '/export type { Address } from ".\/@openzeppelin\/contracts\/utils\/Address";/d' ./src/typechain-types/index.ts
+sed -i '/export { Address__factory } from ".\/factories\/@openzeppelin\/contracts\/utils\/Address__factory";/d' ./src/typechain-types/index.ts
+
 # compile the typechain files in sdk-core
 tsc -p tsconfig.typechain.json
