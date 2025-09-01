4e3bf877b6b7043a2ce1be17af5fd335d0a8018d
Didi <git@d10r.net>
2025-09-01 11:40:14 +0200

    [AUTOMATION] deploy autowrap with burned owner keys, switch to hardhat-verify (#2104)

--- packages/sdk-core/hardhat.config.ts
+++ packages/sdk-core/hardhat.config.ts
@@ -7,6 +7,7 @@ import {
     TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD,
 } from "hardhat/builtin-tasks/task-names";
 import { config as dotenvConfig } from "dotenv";
+import { execSync } from "child_process";
 dotenvConfig();
 
 // The built-in compiler (auto-downloaded if not yet present) can be overridden by setting SOLC
@@ -15,14 +16,19 @@ subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD).setAction(
     async (args, hre, runSuper) => {
         console.log("subtask TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD");
         if (process.env.SOLC !== undefined) {
+            const versionOutput = execSync(`${process.env.SOLC} --version`, { encoding: 'utf8' }).trim();
+            const longVersion = versionOutput.match(/Version: (.+)/)?.[1] || `${args.solcVersion}+commit.unknown.Linux.g++`;
             console.log(
                 "Using Solidity compiler set in SOLC:",
-                process.env.SOLC
+                process.env.SOLC,
+                ", longVersion:",
+                longVersion
             );
             return {
                 compilerPath: process.env.SOLC,
                 isSolcJs: false, // false for native compiler
                 version: args.solcVersion,
+                longVersion,
             };
         } else {
             // fall back to the default
