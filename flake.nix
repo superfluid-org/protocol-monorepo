a87270ad2e5c03e8dfe7a09eb17eb617c941057e
didi <git@d10r.net>
2026-06-17 18:04:27 +0200

    Switch nix flake to Foundry 1.7.1
    Track foundry.nix/stable (dev merge had reverted the pin).

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     foundry = {
-      # Fix to commit on Sep 17, 2025, for now
-      url = "github:shazow/foundry.nix/e632b06dc759e381ef04f15ff9541f889eda6013";
+      url = "github:shazow/foundry.nix/stable";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
